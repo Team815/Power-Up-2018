ecb6c6ac9dd5202b5be12822886cb9527d353e91
Elijah Gutierrez <lego.maniac@ymail.com>
2018-02-15 20:30:18 -0500

    WIP - Adding PID control to the drive

--- Drive.java
+++ Drive.java
@@ -2,22 +2,39 @@ package org.usfirst.frc.team815.robot;
 
 import org.usfirst.frc.team815.robot.Controller.ButtonName;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 public class Drive {
 	private MecanumDrive drive;
+	private ADXRS450_Gyro gyro;
+	private DriveOutput driveOutput;
+	private PIDController driveController;
 	private double speedMultiplier = 1;
 	private static final double MIN_MULTIPLIER = 0.2;
 	private static final double MAX_MULTIPLIER = 1;
 	private static final double MULTIPLIER_INCREMENT = 0.01;
+	
+	private class DriveOutput implements PIDOutput {
+
+		@Override
+		public void pidWrite(double output) {
+		}
+		
+	}
 	
 	public Drive(int frontRightId, int rearRightId, int frontLeftId, int rearLeftId) {
 		WPI_VictorSPX talonFrontRight = new WPI_VictorSPX(frontRightId);
 		WPI_VictorSPX talonRearRight = new WPI_VictorSPX(rearRightId);
 		WPI_VictorSPX talonFrontLeft = new WPI_VictorSPX(frontLeftId);
 		WPI_VictorSPX talonRearLeft = new WPI_VictorSPX(rearLeftId);
-    	  	
+		gyro = new ADXRS450_Gyro();
     	drive = new MecanumDrive(talonFrontLeft, talonRearLeft, talonFrontRight, talonRearRight);
+    	driveController = new PIDController(0.003, 0.0, 0.0, gyro, driveOutput);
+    	driveController.enable();
 	}
 	
 	public void SetMaxSpeed(Controller controller) {
@@ -30,26 +47,7 @@ public class Drive {
 		drive.setMaxOutput(speedMultiplier);
     }
 	
-	public void Update(double horizontal, double vertical, double rotation, double gyroValue) {
-		/*
-		final double QUARTER = Math.PI / 2;
-		final double MAX_MULTIPLIER = 2;
-		
-		double horizontalSign = Math.signum(horizontal);
-		double verticalSign = Math.signum(vertical);
-		
-    	double magnitude = Math.sqrt(Math.pow(horizontal, 2) + Math.pow(vertical, 2));
-    	double angle = vertical == 0 ? 0 : Math.atan(Math.abs(horizontal) / Math.abs(vertical));
-    	double percent = angle / QUARTER;
-    	double multiplier = percent * (MAX_MULTIPLIER - 1) + 1;
-    	
-    	magnitude *= multiplier;
-    	
-    	horizontal = Math.min(1, magnitude * Math.sin(angle));
-    	horizontal *= horizontalSign;
-    	vertical = Math.min(1, magnitude * Math.cos(angle));
-    	vertical *= verticalSign;
-    	*/
-    	drive.driveCartesian(horizontal, vertical, rotation, gyroValue);
+	public void Update(double horizontal, double vertical, double rotation) {
+    	drive.driveCartesian(horizontal, vertical, rotation);
 	}
 }
--- Robot.java
+++ Robot.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team815.robot;
 
-import org.usfirst.frc.team815.robot.Autonomous.State;
 import org.usfirst.frc.team815.robot.Controller.AnalogName;
 import org.usfirst.frc.team815.robot.Controller.ButtonName;
 import org.usfirst.frc.team815.robot.Dpad.Direction;
@@ -19,18 +18,13 @@ import edu.wpi.first.wpilibj.Relay;
  */
 public class Robot extends IterativeRobot {
 	Controller controller0 = new Controller(0);
-	//Controller controller1 = new Controller(1);
 	Controller controllerElevator;
 	Controller controllerTilt;
 	Controller controllerDrive;
-	//Switchboard switchboard = new Switchboard(2);
 	Drive drive = new Drive(4, 7, 0, 3);
-	Relay lightRelay = new Relay(0, Relay.Direction.kForward);
-	Gyro gyro = new Gyro(1);
-	Autonomous auto = new Autonomous(gyro, lightRelay);
+	Autonomous auto = new Autonomous();
 	Elevator elevator = new Elevator(5,6);
 	Tilt tilt = new Tilt(2);
-	//CameraServer server = CameraServer.getInstance();
 	
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -46,7 +40,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		gyro.SetPlayerAngle();
 	}
 	
 	/**
@@ -54,15 +47,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		
-		auto.Update();
-		
-		double horizontal = auto.GetHorizontal();
-		double vertical = auto.GetVertical();
-		double rotation = gyro.GetCompensation();
-		double gyroValue = auto.GetState() == State.Positioning ? gyro.GetAngle() : 0;
-		
-		drive.Update(horizontal, vertical, rotation, gyroValue);
 	}
 	
 	/**
@@ -70,9 +54,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopInit(){
-		
-		gyro.ResetTargetAngle();
-		lightRelay.set(Relay.Value.kOff);
 		
 		controllerDrive = controller0;
 		controllerElevator = controller0;
@@ -145,21 +126,12 @@ public class Robot extends IterativeRobot {
 		// Gyro Section
 		
 		if(controllerDrive.WasClicked(ButtonName.B)) {
-			gyro.SetPlayerAngle();
+			drive.SetPlayerAngle();
 		}
-		
-		/*
-		if(controllerDrive.WasClicked(ButtonName.Select)) {
-			gyro.Calibrate();
-			gyro.SetPlayerAngle();
-		}
-		*/
 		
 		if(controllerDrive.JustZeroed(AnalogName.RightJoyX)){
-			gyro.ResetTargetAngle();
+			drive.ResetTargetAngle();
 		}
-		
-		gyro.Update(controllerDrive.GetValue(AnalogName.RightJoyX) != 0);
 		
 		// Speed Control Section
 		
@@ -170,28 +142,11 @@ public class Robot extends IterativeRobot {
 		
 		// Drive Section
 		
-		double horizontal = 0;
-		double vertical = 0;
-		double rotation = 0;
-		double gyroValue = 0;
+		double horizontal = controllerDrive.GetValue(AnalogName.LeftJoyX);		
+		double vertical = -controllerDrive.GetValue(AnalogName.LeftJoyY);
+		double rotation = controllerDrive.GetValue(AnalogName.RightJoyX);
 		
-		if(!controllerDrive.IsToggled(ButtonName.Select)) {
-			if(controllerDrive.IsToggled(ButtonName.X)) {
-				auto.Update();
-				horizontal = auto.GetHorizontal();
-				vertical = auto.GetVertical();
-				rotation = gyro.GetCompensation();
-				gyroValue = 0;
-			} else {
-				horizontal = controllerDrive.GetValue(AnalogName.LeftJoyX);
-				vertical = -controllerDrive.GetValue(AnalogName.LeftJoyY);
-				rotation = controllerDrive.GetValue(AnalogName.RightJoyX);
-				rotation = rotation == 0 ? gyro.GetCompensation() : rotation;
-				gyroValue = controllerDrive.IsToggled(ButtonName.A) ? 0 : gyro.GetAngle();
-			}
-		}
-		
-		drive.Update(horizontal, vertical, rotation, gyroValue);
+		drive.Update(horizontal, vertical, rotation);
 	}
 	
 	/**
