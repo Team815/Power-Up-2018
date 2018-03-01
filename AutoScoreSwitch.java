5044818ddfb2d7202195e7a8d6743174dd439d10
Elijah Gutierrez <lego.maniac@ymail.com>
2018-03-01 18:30:05 -0500

    Added code to use FMS data and adjust auton routine accordingly, cleaned up other autoSwitch code

--- AutoScoreSwitch.java
+++ AutoScoreSwitch.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team815.robot;
 
 import org.usfirst.frc.team815.robot.Claw.RollerDirection;
-import org.usfirst.frc.team815.robot.Controller.AnalogName;
 import org.usfirst.frc.team815.robot.Elevator.PresetTarget;
 import org.usfirst.frc.team815.robot.Tilt.State;
 
@@ -9,7 +8,8 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class AutoScoreSwitch extends Autonomous {
-	private static final float DRIVE_TIME_LIMIT = 2;	// Experiment with this value
+	private static final float DRIVE_VERTICAL_TIME = 2;	// Experiment with this value
+	private static final float DRIVE_HORIZONTAL_TIME = 1;
 	
 	public AutoScoreSwitch(Gyro gyroIn, Claw claw, Tilt tilt, Elevator elevator) {
 		super(gyroIn);
@@ -23,67 +23,75 @@ public class AutoScoreSwitch extends Autonomous {
 
 	@Override
 	public void StartAuto() {
-		//claw.openClaw();
 		routineTimer.start();
 	}
 
 	@Override
 	public void Update() {
-		if (routineTimer.get() > 0) {
-			action = setAction();
-			
-			switch (action) {
-			case TILT_FORWARD:
-				if(tilt.state == State.DOWN)
-					tilt.StartTilting();
-				tilt.Update();
-				break;
-			case APPROACH_SWITCH:
-				if(driveTimer.get() == 0)
+		action = setAction();
+
+		switch (action) {
+		case TILT_FORWARD:
+			if(tilt.state == State.DOWN)
+				tilt.StartTilting();
+			tilt.Update();
+			break;
+		case APPROACH_SWITCH:
+			if(driveTimer.get() == 0)
+				driveTimer.start();
+			else {
+				if(driveTimer.get() > DRIVE_VERTICAL_TIME) {
+					horizontal = 0;
+					vertical = 0;
+					rotation = 0;
 					driveTimer.start();
-				else {
-					if(driveTimer.get() > DRIVE_TIME_LIMIT) {
-						horizontal = 0;	
-						vertical = 0;
-						rotation = 0;
-						driveTimer.start();
-						driveTimer.reset();
+					driveTimer.reset();
+				} else if(driveTimer.get() < DRIVE_HORIZONTAL_TIME) {
+					if(GameLayout.charAt(0) == 'L') {
+						horizontal = -1;
+					} else if(GameLayout.charAt(0) == 'R') {
+						horizontal = 1;
+					} else {
+						horizontal = 0;
 					}
-					else {
-						horizontal = 0;	
-						vertical = 1;
-						rotation = 0;
-					}
+					vertical = 0;
+					rotation = 0;
+				} else if(driveTimer.get() < DRIVE_VERTICAL_TIME) {
+					horizontal = 0;
+					vertical = 1;
+					rotation = 0;
 				}
-				break;
-			case RAISE_ELEVATOR:
-				elevator.SetPresetTarget(PresetTarget.SWITCH);
-				break;
-			case DROP_POWERCUBE:
-				claw.setRollerDirection(RollerDirection.FORWARD);
-				break;
-			case STOP:
-				claw.setRollerDirection(RollerDirection.STOPPED);
-				routineTimer.stop();
-				routineTimer.reset();
-				driveTimer.stop();
-				driveTimer.reset();
-				break;
 			}
+			break;
+		case RAISE_ELEVATOR:
+			elevator.SetPresetTarget(PresetTarget.SWITCH);
+			break;
+		case DROP_POWERCUBE:
+			claw.setRollerDirection(RollerDirection.FORWARD);
+			break;
+		case STOP:
+			claw.setRollerDirection(RollerDirection.STOPPED);
+			routineTimer.stop();
+			routineTimer.reset();
+			driveTimer.stop();
+			driveTimer.reset();
+			break;
 		}
 	}
 
 	@Override
 	protected Action setAction() {		// Need to combine actions after testing to get under 15 seconds
-		if(routineTimer.get() < 8)
+		if(routineTimer.get() == 0)
+			return Action.STOP;
+		else if(routineTimer.get() < 8)
 			return Action.TILT_FORWARD;
-//		else if(routineTimer.get() < 10)
-//		return Action.RAISE_ELEVATOR;
-//		else if(routineTimer.get() < 12)
-//			return Action.APPROACH_SWITCH;
-//		else if(routineTimer.get() < 14)
-//			return Action.DROP_POWERCUBE;
-		else if(routineTimer.get() >= 14 || routineTimer.get() == 0)
+		else if(routineTimer.get() < 10)
+		return Action.RAISE_ELEVATOR;
+		else if(routineTimer.get() < 12)
+			return Action.APPROACH_SWITCH;
+		else if(routineTimer.get() < 14)
+			return Action.DROP_POWERCUBE;
+		else if(routineTimer.get() >= 14)
 			return Action.STOP;
 		else return Action.STOP;
 	}
