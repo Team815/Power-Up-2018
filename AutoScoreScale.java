88935874cd6c3107d76b14728a685ccfc9b64388
Cobb <ACOBB6@ford.com>
2018-03-07 16:25:09 -0500

    Cleaned up AutoScoreScale and finished 50% score cases

--- AutoScoreScale.java
+++ AutoScoreScale.java
@@ -12,6 +12,7 @@ public class AutoScoreScale extends Autonomous {
 	private static final Movement CLOSE = new Movement(0.5, 00.0, 00.0, 2.5);
 	private static final Movement FAR   = new Movement(0.25, 90.0, 00.0, 5.0);
 	private static final double H_FACTOR = 2.8;
+	private final char target = GameLayout.charAt(1);
 	
 	public AutoScoreScale(Gyro gyroIn, Claw claw, Tilt tilt, Elevator elevator, SwitchState switchStateIn) {
 		super(gyroIn, switchStateIn);
@@ -23,19 +24,17 @@ public class AutoScoreScale extends Autonomous {
 
 	@Override
 	public void StartAuto() {
-		char target = GameLayout.charAt(1);
 		if(switchState == SwitchState.HALF_SCORE_SCALE_LEFT || switchState == SwitchState.HALF_SCORE_SCALE_RIGHT) {
 			speed = HALF.SPEED;
 			angleStart = HALF.ANGLE_START;
 			angleEnd = HALF.ANGLE_END;
 			timeout = HALF.TIMEOUT;
-		} else
-			if(switchState == SwitchState.SCORE_SCALE_RIGHT && target == 'R'
-		       || switchState == SwitchState.SCORE_SCALE_LEFT  && target == 'L') {
+		} else if(scoreOnNearSide(target)) {
 			speed = CLOSE.SPEED;
 			angleStart = CLOSE.ANGLE_START;
 			angleEnd = CLOSE.ANGLE_END;
 			timeout = CLOSE.TIMEOUT;
+		/* !!! Movement for this case has not been configured yet !!! */
 		} else {
 			speed = FAR.SPEED;
 			angleStart = FAR.ANGLE_START;
@@ -55,9 +54,12 @@ public class AutoScoreScale extends Autonomous {
 			boolean isStraight = IsStraight();
 			boolean atScale = AtSwitch();
 			if(isStraight && atScale) {
+				if(doScore()) {
 				elevator.SetPresetTarget(PresetTarget.SCALE);
 				action = Action.RAISE_ELEVATOR;
 				timer.reset();
+				}
+				else action = Action.STOP;
 			}
 			break;
 		case RAISE_ELEVATOR:
@@ -106,4 +108,29 @@ public class AutoScoreScale extends Autonomous {
 			return false;
 		}
 	}
+	
+	private boolean scoreOnNearSide(char target) {
+		switch(switchState) {
+		case HALF_SCORE_SCALE_LEFT:
+		case SCORE_SCALE_LEFT:
+			if(target == 'L')
+				return true;
+			else return false;
+		case HALF_SCORE_SCALE_RIGHT:
+		case SCORE_SCALE_RIGHT:
+			if(target == 'R')
+				return true;
+			else return false;
+		default:
+			return true;
+		}
+	}
+	
+	private boolean doScore() {
+		if((switchState == SwitchState.HALF_SCORE_SCALE_RIGHT || switchState == SwitchState.HALF_SCORE_SCALE_LEFT) 
+			&& !scoreOnNearSide(target))
+			return false;
+		else return true;
+	}
+	
 }
